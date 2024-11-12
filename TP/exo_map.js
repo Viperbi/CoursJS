d014fc297e783749aee9f2672d3e2bb978681c54
Viperbi <nickels.florian@gmail.com>
2024-11-12 16:42:09 +0100

    Real style fixing

--- TP/exo_map.js
+++ TP/exo_map.js
@@ -8,7 +8,12 @@ console.log(tab);
 tab.map((p) => {
 	p.style.cursor = "pointer";
 	p.style.boxShadow = "10px 5px 100px lightblue";
-	p.style.color = "grey";
+	p.style.color = "purple";
+	p.style.border = "thick solid black";
+	p.style.padding = "5px";
+	p.style.backgroundColor = "black";
+	p.style.borderRadius = "100px";
+	p.style.borderColor = "black";
 	p.textContent = "Ouai salut ouai";
 	p.addEventListener("click", () => {
 		alert("WARNING");
