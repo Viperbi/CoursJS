db3db3313c4d344255d382f02ed21eeb67a6a60f
Viperbi <nickels.florian@gmail.com>
2024-11-13 09:47:24 +0100

    changement style exo_titre

--- TP/exo_titre.js
+++ TP/exo_titre.js
@@ -2,6 +2,7 @@ let clicked = true;
 
 let titre = document.getElementsByTagName("h1");
 titre[0].style.cursor = "pointer";
+titre[0].style.textAlign = "center";
 
 console.log(titre[0]);
 titre[0].addEventListener("click", () => {
