fd74984813fdf620cb2375a4a68d5609a6a5e7c9
Viperbi <nickels.florian@gmail.com>
2024-11-25 10:10:14 +0100

    Ajout de commentaire dans IMC.js

--- SuiteJS/IMC.js
+++ SuiteJS/IMC.js
@@ -4,9 +4,11 @@ class IMC {
 		this.poids = poids;
 		this.taille = taille;
 	}
+	//Fonction permettant de calculer l'IMC d'une personne à partir de son poids et de sa taille.
 	calculIMC(taille, poids) {
 		return (poids / (taille * taille)).toFixed(2);
 	}
+	//Méthode pour afficher les différentes informations de la personne.
 	displayinfo() {
 		console.log(
 			`${this.nom} (${this.poids}kg, ${
@@ -15,7 +17,7 @@ class IMC {
 		);
 	}
 }
-
+//Tableau contenant les données de différentes personnes
 const list = [
 	new IMC("Sébastien chabal", 135, 1.7),
 	new IMC("Escaladeuse", 45, 1.68),
@@ -24,7 +26,7 @@ const list = [
 	new IMC("Colonel Clock", 200, 1.75),
 	new IMC("J0siane de la Vega", 99, 1.55),
 ];
-
+//Boucle permettant de parcourir le tableau list et d'afficher les infos de chaque personne.
 for (let i = 0; i < list.length; i++) {
 	list[i].displayinfo();
 }
